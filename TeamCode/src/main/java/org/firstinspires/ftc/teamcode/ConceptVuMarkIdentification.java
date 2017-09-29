fb771cd199c267b3307c7c9eb892337edbe79ef3
qingyuan peng <qingyuanpeng@gfs.org>
2017-09-29 16:39:11 -0400

    add vumark code in the team code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuMarkIdentification.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuMarkIdentification.java
@@ -26,12 +26,13 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaNavigation;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
@@ -66,7 +67,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
  */
 
 @Autonomous(name="Concept: VuMark Id", group ="Concept")
-//@Disabled
+@Disabled
 public class ConceptVuMarkIdentification extends LinearOpMode {
 
     public static final String TAG = "Vuforia VuMark Sample";
@@ -103,8 +104,7 @@ public class ConceptVuMarkIdentification extends LinearOpMode {
          * Once you've obtained a license key, copy the string from the Vuforia web site
          * and paste it in to your code onthe next line, between the double quotes.
          */
-        parameters.vuforiaLicenseKey = "ASGxdl//////AAAAGSv6/70u4k60lnq7i2KxDlUuD5UeTMirttRwBuiLVuS0pUaIR9TZOPDN0FH88OZ5q9lBryWrPX3SI5ATzLpDny4ajKp4pBiAigqKTkMMPZeSxlZN7gVPcImrgfO+1/5JSY+PMb3i3+elUVtQlsitzGE+m41x3RsU7FCg2HGpdns5L2bKGKj3fFJu5pdRGTKcTFPXXhcZpdtS5rJJlaGnd3fDW4DCYl7dIycTl+XWi0kNslWOgzFkLMZnpqIaswTsjEBJYB2dZdPRt0PPXCi++nAKoiU4qdE5i6hIz9UhQt/ZCQTUdqVcAHrYMIoTf5XbntTwbtHvxXPwUWV3NY5z0kjNJT7qDE/kUYYebGNUoxLT";
-        ;
+        parameters.vuforiaLicenseKey = "ASGxdl//////AAAAGSv6/70u4k60lnq7i2KxDlUuD5UeTMirttRwBuiLVuS0pUaIR9TZOPDN0FH88OZ5q9lBryWrPX3SI5ATzLpDny4ajKp4pBiAigqKTkMMPZeSxlZN7gVPcImrgfO+1/5JSY+PMb3i3+elUVtQlsitzGE+m41x3RsU7FCg2HGpdns5L2bKGKj3fFJu5pdRGTKcTFPXXhcZpdtS5rJJlaGnd3fDW4DCYl7dIycTl+XWi0kNslWOgzFkLMZnpqIaswTsjEBJYB2dZdPRt0PPXCi++nAKoiU4qdE5i6hIz9UhQt/ZCQTUdqVcAHrYMIoTf5XbntTwbtHvxXPwUWV3NY5z0kjNJT7qDE/kUYYebGNUoxLT\n";
 
         /*
          * We also indicate which camera on the RC that we wish to use.
